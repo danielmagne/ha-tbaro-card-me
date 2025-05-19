4680f4bbca8964607abf5558f3d55e157b53a400
trollix <dr.chandra@laposte.net>
2025-05-19 15:17:54 +0200

    release v0.5.13

--- src/ha-tbaro-card.ts
+++ src/ha-tbaro-card.ts
@@ -195,7 +195,7 @@ render() {
   const weatherYOffset = gaugeAngle === 180 ? -90 : 0;
   const iconYOffset = gaugeAngle === 180 ? -90 : 0;
   const iconX = cx - 25;
-  const iconY = (gaugeAngle === 180 ? cy : cy ) + iconYOffset;
+  const iconY = (gaugeAngle === 180 ? cy : cy+5 ) + iconYOffset;
   const labelY = (gaugeAngle === 180 ? cy + 40 : cy + 60);
   const pressureY = (gaugeAngle === 180 ? cy + 60 : cy + 85);
 
