78f3873dc399877a97b9b14f51196042869cf800
trollix <dr.chandra@laposte.net>
2025-06-02 16:25:37 +0200

    release v0.6.4-a11

--- src/ha-tbaro-card.ts
+++ src/ha-tbaro-card.ts
@@ -361,7 +361,7 @@ render() {
       position: absolute;
       left:${iconX}px;
       top:${iconY}px;
-      transform: translate(460%, -20%);/* centre l’icône */
+      transform: translate(480%, -20%);/* centre l’icône */
       color:${tick_color};
     "
   ></ha-icon>
