b0fc8fcc6812388dfba0cc011633e1bfb33b0703
danielmagne <124636873+danielmagne@users.noreply.github.com>
2025-09-29 20:35:36 +0200

    Add files via upload

--- dist/ha-tbaro-card.js
+++ dist/ha-tbaro-card.js
@@ -63,7 +63,15 @@ const ct={attribute:!0,type:String,converter:y,reflect:!1,hasChanged:A},dt=(t=ct
           ${P}
           ${T}
           ${this.config.show_label?B`<text x="${h}" y="${A}" font-size="14" class="label">${w}</text>`:q}
-          <text x="${h}" y="${C}" font-size="22" font-weight="bold" class="label">
+          <text
+            x="${h}"
+            y="${C}"
+            font-size="22"
+            font-weight="bold"
+            class="label"
+            style="cursor:pointer;"
+            @click=${()=>this.hass.callService("browser_mod.popup",{title:"Sensor Entity",card:{type:"entity",entity:this.config.entity}})}
+          >
             ${"mm"===this.config.unit?Math.floor(t).toFixed(0)+" mm":"in"===this.config.unit?Math.floor(t).toFixed(0)+" inHg":Math.floor(t).toFixed(0)+" hPa"}
           </text>
         </svg>`}
